b2c7d7172198f09994c9442d593ffbac43725f6a
yellow-fellow <huangshaohang.98@gmail.com>
2020-06-22 13:00:09 +0800

    Added multiple filters & blur functions

--- jsTutorial.js
+++ jsTutorial.js
@@ -1,15 +1,20 @@
 var image1;
 var image2;
+var og1;
+var og2;
+
 function upload1() {
     var fileInput = document.getElementById('fginput');
     var imgCanvas = document.getElementById("d1");
     image1 = new SimpleImage(fileInput);
+    og1 = new SimpleImage(fileInput);
     image1.drawTo(imgCanvas);
 }
 function upload2() {
     var imgCanvas2 = document.getElementById('d2');
     var fileInput = document.getElementById('bginput');
     image2 = new SimpleImage(fileInput);
+    og2 = new SimpleImage(fileInput);
     image2.drawTo(imgCanvas2);
 }
 
@@ -76,5 +81,131 @@ function gray2() {
 
 }
 
+function red1() {
 
+    for (var pixel of image1.values()){
+        pixel.setRed(255);
+    }
+    var imgCanvas = document.getElementById("d1");
+    image1.drawTo(imgCanvas);
 
+}
+
+function red2() {
+
+    for (var pixel of image2.values()){
+        pixel.setRed(255);
+    }
+    var imgCanvas = document.getElementById("d2");
+    image2.drawTo(imgCanvas);
+
+}
+
+function blur1() {
+    
+
+    function ensureInImage (coordinate, size) {
+        // coordinate cannot be negative
+        if (coordinate < 0) {
+            return 0;
+        }
+        // coordinate must be in range [0 .. size-1]
+        if (coordinate >= size) {
+            return size - 1;
+        }
+        return coordinate;
+    }
+    
+    function getPixelNearby (image1, x, y, diameter) {
+        var dx = Math.random() * diameter - diameter / 2;
+        var dy = Math.random() * diameter - diameter / 2;
+        var nx = ensureInImage(x + dx, image1.getWidth());
+        var ny = ensureInImage(y + dy, image1.getHeight());
+        return image1.getPixel(nx, ny);
+    }
+    
+    for (var pixel of image1.values()) {
+        var x = pixel.getX();
+        var y = pixel.getY();
+        if (Math.random() > 0.5) {
+            var other = getPixelNearby(image1, x, y, 10);
+            image1.setPixel(x, y, other);
+        }
+        else {
+            image1.setPixel(x, y, pixel);
+        }
+    }
+
+    var imgCanvas = document.getElementById("d1");
+    image1.drawTo(imgCanvas);
+}
+
+function blur2() {
+    
+
+    function ensureInImage (coordinate, size) {
+        // coordinate cannot be negative
+        if (coordinate < 0) {
+            return 0;
+        }
+        // coordinate must be in range [0 .. size-1]
+        if (coordinate >= size) {
+            return size - 1;
+        }
+        return coordinate;
+    }
+    
+    function getPixelNearby (image2, x, y, diameter) {
+        var dx = Math.random() * diameter - diameter / 2;
+        var dy = Math.random() * diameter - diameter / 2;
+        var nx = ensureInImage(x + dx, image2.getWidth());
+        var ny = ensureInImage(y + dy, image2.getHeight());
+        return image2.getPixel(nx, ny);
+    }
+    
+    for (var pixel of image2.values()) {
+        var x = pixel.getX();
+        var y = pixel.getY();
+        if (Math.random() > 0.5) {
+            var other = getPixelNearby(image2, x, y, 10);
+            image2.setPixel(x, y, other);
+        }
+        else {
+            image2.setPixel(x, y, pixel);
+        }
+    }
+
+    var imgCanvas = document.getElementById("d2");
+    image2.drawTo(imgCanvas);
+}
+
+function reset() {
+
+    var imgCanvas1 = document.getElementById("d1");
+    og1.drawTo(imgCanvas1);
+
+    var imgCanvas2 = document.getElementById("d2");
+    og2.drawTo(imgCanvas2);
+}
+
+function rainbow1() {
+    var imgCanvas = document.getElementById('d1');
+
+    for (var pixel of image1.values()){
+        var x = pixel.getX();
+        var y = pixel.getY();
+
+        if (x <= (image1.getHeight()/3)){
+            pixel.setRed(255);
+        }
+        else if (x <= (image1.getHeight()/3)*2){
+            pixel.setGreen(255);
+        }
+        else{
+            pixel.setBlue(255);
+        }
+
+    }
+
+    image1.drawTo(imgCanvas);
+}
